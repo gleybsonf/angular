78caf674298ac53bd8698da685889cf13632ba9c
Gleybson Farias <gleybson.farias@gmail.com>
2025-01-21 21:42:20 -0300

    criando componente home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TaskManagementListComponent } from "./feactures/task-management/pages/task-management-list/task-management-list.component";
+import { HomeComponent } from './feactures/home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TaskManagementListComponent],
+  imports: [RouterOutlet, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

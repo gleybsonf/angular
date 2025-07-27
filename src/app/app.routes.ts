36f08c28b6a846c6344583f57dbf55cc25e4ac9c
Gleybson Farias <gleybson.farias@gmail.com>
2025-07-27 17:46:01 -0300

    feacture: developing login component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+import { LoginComponent } from "./login/login.component";
+import { GLOBAL_CONSTANTS } from "./core/constants/global.constants";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "", redirectTo: "login", pathMatch: "full" }, // Redireciona a raiz para o login
+  { path: "login", component: LoginComponent, title: GLOBAL_CONSTANTS.LOGIN }, // Rota pública de login
+];

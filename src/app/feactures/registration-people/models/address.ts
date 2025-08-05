a88cc6cbfed8819f5bc9798508270faa825c293d
Gleybson Farias <gleybson.farias@gmail.com>
2025-08-04 23:02:21 -0300

    refactor: ajusts address class

--- src/app/feactures/registration-people/models/address.ts
+++ src/app/feactures/registration-people/models/address.ts
@@ -1,21 +1,43 @@
 export class Address {
+  public postalCode: string;
+  public country: string;
+  public complement?: string;
+  public district?: string;
+  public street: string;
+  public number: string;
+  public city: string
+  public state: string;
+
+
   constructor(
-    public street: string,
-    public number: string,
-    public city: string,
-    public state: string,
-    public postalCode: string,
-    public country: string,
-    public complement?: string,
-    public district?: string,
-  ) {}
+      street: string,
+      number: string,
+      city: string,
+      state: string,
+      postalCode: string,
+      country: string,
+      complement?: string,
+      district?: string,
+      isValid: boolean = true,
+    ) {
+    this.city = city;  
+      this.state = state;
+      this.street = street;
+      this.number = number;
+      this.postalCode = postalCode;
+      this.country = country;
+      this.complement = complement;
+      this.district = district;
+      
+
+    }
 
   get fullAddress(): string {
     const base = `${this.street}, ${this.number} - ${this.city}/${this.state}`;
     return this.complement ? `${base} (${this.complement})` : base;
   }
 
-  isValid(): boolean {
+  private isValid(): boolean {
     return !!(this.street && this.number && this.city && this.state && this.postalCode);
   }
 }

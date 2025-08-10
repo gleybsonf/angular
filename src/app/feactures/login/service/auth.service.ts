7fd871d64140924a937e00b326b768e880fae458
Gleybson Farias <gleybson.farias@gmail.com>
2025-08-10 12:15:41 -0300

    refactor: ajustes gerais

--- src/app/feactures/login/service/auth.service.ts
+++ src/app/feactures/login/service/auth.service.ts
@@ -15,8 +15,8 @@ export class AuthService {
 
   }
 
-  protected onLogin(email: string, password: string): Observable<IUser>{
-    return of(userMock).pipe(delay(5000))
+  public onLogin(email: string, password: string): Observable<IUser| null>{
+    return of(null/* userMock */).pipe(delay(5000))
     //this.http.post<IUser>(`${this.baseUrl}/login`, { email, password });
   }
 }
